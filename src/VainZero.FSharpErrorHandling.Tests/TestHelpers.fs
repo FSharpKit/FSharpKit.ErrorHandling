d455f34552383e3561b501ee834db411e5b3e088
vain0x <vainzerox@gmail.com>
2017-12-02 21:16:17 +0900

    Migrate Result.Tests from Persimmon to Expecto

--- src/VainZero.FSharpErrorHandling.Tests/TestHelpers.fs
+++ src/VainZero.FSharpErrorHandling.Tests/TestHelpers.fs
@@ -6,16 +6,24 @@ open System
 module TestHelpers =
   open Expecto
 
-  let is expected actual =
-    Expect.equal actual expected "These should equal."
+  let inline is expected actual =
+    Expect.equal actual expected "These should equal"
 
   let trap f =
     try
       f () |> ignore
-      failtest "It should throw."
+      failtest "It should throw"
     with
     | e ->
       e
+
+  let parameterize name parameters run =
+    testList name
+      [
+        for (i, parameter) in parameters |> Seq.indexed do
+          let name = sprintf "#%d: %A" (i + 1) parameter
+          yield testCase name (fun () -> run parameter)
+      ]
 
 [<Sealed>]
 type CountDisposable() =
